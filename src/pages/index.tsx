f1d1a4ec3c541249778fd241e11bf804bde47924
Luciano Junior <lucianomoraisjr@gmail.com>
2022-03-22 14:15:41 -0300

    finished modulo 2 aula 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,38 @@
-import Head from 'next/head'
+import {Button, Flex,Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <h1>Dashbord</h1>
+    <Flex 
+    w="100vw" 
+    h="100vh" 
+    align="center" 
+    justify="center"
+  >
+    <Flex
+      as="form"
+      w="100%"
+      maxWidth={360}
+      bg="gray.800"
+      p="8"
+      borderRadius="8"
+      flexDir="column"
+    >
+      <Stack spacing="4">
+        <Input type="email" name="email" label="E-mail" />
+        <Input type="password" name="password" label="Senha" />
+      </Stack>
+
+      <Button 
+        type="submit" 
+        mt="6" 
+        colorScheme="pink" 
+        size="lg"
+      >
+        Entrar
+      </Button>
+    </Flex>
+  </Flex>
   )
 }
