317211903ed2b0c8db35794b37a5edcd59e3cf6c
Luciano Junior <lucianomoraisjr@gmail.com>
2022-03-29 10:23:38 -0300

    finish  IV-01-04-09

--- src/components/SideBar/NavLink.tsx
+++ src/components/SideBar/NavLink.tsx
@@ -1,23 +1,22 @@
-import {  Icon, Text, Link as ChakraLink, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
+import { Icon, Text, Link as ChakraLink, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
 import Link from "next/link";
 import { ElementType } from "react";
+import { ActiveLink } from "../ActiveLink";
 
 
 
-interface NavLinkProrps extends ChakraLinkProps{
-    icon :ElementType,
-    children:string
-    href:string
+interface NavLinkProrps extends ChakraLinkProps {
+    icon: ElementType,
+    children: string
+    href: string
 }
-export function NavLink({href, icon,children,...rest}:NavLinkProrps) {
+export function NavLink({ href, icon, children, ...rest }: NavLinkProrps) {
     return (
-       <Link href={href} passHref >
-        <ChakraLink display="flex" alignItems='center' {...rest}>
-            
-            <Icon as={icon} fontSize="20" />
-            <Text ml="4" fontWeight="medium">{children}</Text>
-        </ChakraLink>
-       </Link>
-
-    )
+        <ActiveLink href={href} passHref >
+            <ChakraLink display="flex" alignItems='center' {...rest}>
+                <Icon as={icon} fontSize="20" />
+                <Text ml="4" fontWeight="medium">{children}</Text>
+            </ChakraLink>
+        </ActiveLink>
+        )
 }
